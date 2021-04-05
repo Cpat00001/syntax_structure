34e5e93108a2c8caf99ca6ac5c8411bd080764c2
Pat <p171982@gmail.com>
2021-04-05 12:51:36 +0100

    woocommerce shop/product/checkout/paypal

---
+++ exampleThree/page-basket.php
@@ -0,0 +1,8 @@
+<?php get_header();?>
+<div class="container" class="basket">
+    <?php echo do_shortcode("[woocommerce_cart]");  ?>
+</div>
+
+<?php get_footer();?>
+
+?>
---
+++ exampleThree/page-checkout.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<div class="conteiner">
+    <?php echo do_shortcode("[woocommerce_checkout]"); ?>
+</div>
+
+
+<?php get_footer(); ?>
