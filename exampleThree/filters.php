1d11087b6eaf032fbaada0ae83bbfe5fe392e196
Pat <p171982@gmail.com>
2021-04-22 07:39:33 +0100

    some changes

--- exampleThree/filters.php
+++ exampleThree/filters.php
@@ -1,69 +1,4 @@
-    <!-- process form -->
-    <?php
 
-//empty errors
-$radioError = '';
-
-if($_SERVER['REQUEST_METHOD'] == 'GET'){
-    $color = $_GET['color'];
-    $size = $_GET['size'];
-    $destination = $_GET['destination'];
-    $radio = $_GET['relation'];
-    if(empty($_GET['relation'])){
-        $radioError = " ** Check radio button";
-    }else{
-        $radioError = ' ';
-    }
-   var_dump($color);
-   echo "<br>";
-    var_dump($size);
-    echo "<br>";
-    var_dump($destination);
-    echo "<br>";
-    var_dump($radio);
-//prepqre query
-$args_color_size = array(
-    'post_type' => 'product',
-    // 'relation' => 'AND',
-    'tax_query'      => array(
-        'relation' => $radio,
-        array(
-        'taxonomy'        => 'pa_color',
-        'field'           => 'slug',
-        'terms'           =>  $color,
-        'operator'        => 'IN',
-    ),
-    array(
-        'taxonomy' => 'pa_size',
-        'field' => 'slug',
-        'terms' => $size,
-        'operator' => 'IN'
-    ),
-    array(
-        'taxonomy' => 'pa_destination',
-        'field' => 'slug',
-        'terms' => $destination,
-        'operator' => 'IN',
-    ))
-    );
-//execute
-$loop_color = new WP_Query( $args_color_size );
-if ( $loop_color->have_posts() ) {
-    while ( $loop_color->have_posts() ){
-        $loop_color->the_post();?>
-        <h5>Results according to your filters</h5>
-         <?php
-        echo "<p class='filter_confirmation_text'> used filters: " . $_GET['color'] . ' , ' . $_GET['size'] . ' , ' . $_GET ['destination'] . '</p><br><br>';
-        wc_get_template_part( 'content', 'product' );
-
-    } 
-} else {
-    // echo __( 'No products found' );
-}
-wp_reset_postdata();
- }
-// #####
-?>
 
 <!-- html form -->
 <div class="container">
@@ -72,6 +7,7 @@ wp_reset_postdata();
         <h5>choose your filters and search:</h5>
         <!-- form -->
         <form method="GET" action="<?php echo htmlspecialchars($_SERVER[" __FILE__ "]);?>" class="product_filter_form">
+        <input type="hidden" name="search_button" value="true" />
         <div class="row">
             <div class="col-sm-12 col-md-2">
                 <label for="color"><?php _e('Search by color','textdomain') ?></label><br>
@@ -107,8 +43,7 @@ wp_reset_postdata();
             <input type="radio" id="AND" name="relation" value="AND">
             <label for="radio_button_two">Contains ALL parameters</label>
             </div>
-        </div>
-            
+        </div>          
             <br><br>
             <input type="submit" id="filter_search" value="Search" class="btn btn-primary btn-lg btn-block">
         </form>
@@ -122,6 +57,84 @@ wp_reset_postdata();
 </div>
 <div class="container" style="border:2px solid white;margin-bottom:50px;">
 </div>
+
+    <!-- process form -->
+    <?php
+
+//empty errors
+$radioError = '';
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $color = $_GET['color'];
+    $size = $_GET['size'];
+    $destination = $_GET['destination'];
+    $radio = $_GET['relation'];
+    if(empty($_GET['relation'])){
+        $radioError = " ** Check radio button";
+    }else{
+        $radioError = ' ';
+    }
+    $submit = $_GET['search_button'];
+    // var_dump($submit);
+   var_dump($color);
+   echo "<br>";
+    var_dump($size);
+    echo "<br>";
+    var_dump($destination);
+    echo "<br>";
+    var_dump($radio);
+//prepqre query
+$args_color_size_destination = array(
+    'post_type' => 'product',
+    // 'relation' => 'AND',
+    'tax_query'      => array(
+        'relation' => $radio,
+        array(
+        'taxonomy'        => 'pa_color',
+        'field'           => 'slug',
+        'terms'           =>  $color,
+        'operator'        => 'IN',
+    ),
+    array(
+        'taxonomy' => 'pa_size',
+        'field' => 'slug',
+        'terms' => $size,
+        'operator' => 'IN'
+    ),
+    array(
+        'taxonomy' => 'pa_destination',
+        'field' => 'slug',
+        'terms' => $destination,
+        'operator' => 'IN',
+    ))
+    );
+//execute
+$loop_color = new WP_Query( $args_color_size_destination ); ?>
+<h5>Results according to your filters</h5>
+<?php
+if ( $loop_color->have_posts() ) {
+    while ( $loop_color->have_posts() ){
+        $loop_color->the_post();?>
+        <div class="container" style="background-color:peru;border:2px solid violet;margin-bottom:0.25rem;">
+        <?php
+        echo "<p class='filter_confirmation_text'> used filters: " . $_GET['color'] . ' , ' . $_GET['size'] . ' , ' . $_GET ['destination'] . '</p><br><br>';
+        wc_get_template_part( 'content', 'product' );
+        ?>
+        
+        </div>
+        
+<?php
+    } 
+} else { ?>
+    <div class="container">
+     <?php echo __( '<h5>Search for products.So far No products found</h5>' ); ?>
+    </div>
+<?php 
+}
+wp_reset_postdata();
+ }
+// #####
+?>
 
 
 
--- exampleThree/page-clothing.php
+++ exampleThree/page-clothing.php
@@ -1,46 +1,38 @@
 <?php get_header(); ?>
 <?php include 'filters.php'; ?>
-<?php
-    // dzialajaca petla
-    // $args = array('post_type' => 'product','product_cat' => 'clothing','posts_per_page'=> 10);
 
-    // $loop = new WP_Query($args);
-    ?>
-    <!-- <div class="container"> -->
-        <?php
-            // if($loop->have_posts()){
-            //     while($loop->have_posts()){
-            //         $loop->the_post();
-            //         wc_get_template_part( 'content', 'product' );
-            //     } 
-            // } else {
-            //     echo __('<p>Sorry no posts found</p>');
-            // }
+<?php var_dump($submit); ?>
+
+<?php
+// conditional displaying result default list of product or required by user
+if($submit == ''){
+    // echo "<h1>DEFAULT</h1>";
+        $args = array('post_type' => 'product','product_cat' => 'clothing','posts_per_page'=> 10);
+
+        $loop = new WP_Query($args);
         ?>
-    <!-- </div> -->
-
-    <!-- now blok testowy -->
+        <div class="container">
+        <?php
+                if($loop->have_posts()){
+                    while($loop->have_posts()){
+                        ?>
+                        <div style="background-color: rgb(122, 122, 120); border:2px solid grey; margin:5px;padding:10px;">
+                        <?php
+                        $loop->the_post();
+                        wc_get_template_part( 'content', 'product' );
+                        ?>
+                        </div>
+                        <?php
+                    } 
+                } else {
+                    echo __('<p>Sorry no posts found</p>');
+                }
+            ?>
+        </div>
 <?php
-$args = array('post_type' => 'product','product_cat' => 'clothing','posts_per_page'=> 10);
-
-$loop = new WP_Query($args);
+}else{
+    echo " ";
+}
 ?>
-<div class="container">
-<?php
-        if($loop->have_posts()){
-            while($loop->have_posts()){
-                ?>
-                <div style="background-color: rgb(122, 122, 120); border:2px solid grey; margin:5px;padding:10px;">
-                <?php
-                $loop->the_post();
-                wc_get_template_part( 'content', 'product' );
-                ?>
-                </div>
-                <?php
-            } 
-        } else {
-            echo __('<p>Sorry no posts found</p>');
-        }
-    ?>
-</div>
+
 <?php get_footer(); ?>
