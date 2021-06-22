ffbabc78c6f66f85ea93bd91bfeb7f7a6309ae87
Pat <p171982@gmail.com>
2021-06-22 08:28:06 +0100

    some new features/menu

---
+++ js/one.js
@@ -0,0 +1,27 @@
+window.addEventListener("resize",hide_menu);
+
+let dm = document.getElementsByClassName('dynamicmenu')[0]; 
+let db = document.getElementsByClassName('dynamicbutton')[0];
+
+function hide_menu(){
+    if(window.innerWidth < 576){
+        dm.style.display = 'none';
+        db.style.display = 'block';
+        // alert('window less than 576px');
+    }else{
+        // alert('window szerokie');
+        dm.style.display = 'block';
+        db.style.display = 'none';
+    }
+}
+
+// hide show dynamicmenu on click
+db.addEventListener("click",show_hide_dynamic);
+
+function show_hide_dynamic(){
+    if(dm.style.display === "none"){
+        dm.style.display = "block";
+    }else{
+        dm.style.display = "none";
+    }
+}
