0d8637f7c4b301a6bb3b64964517d0875251be43
Pat <p171982@gmail.com>
2021-06-25 10:14:40 +0100

    some js features / css styling

--- js/one.js
+++ js/one.js
@@ -1,13 +1,29 @@
+window.addEventListener("load",check_size);
 window.addEventListener("resize",hide_menu);
 
-let dm = document.getElementsByClassName('dynamicmenu')[0]; 
-let db = document.getElementsByClassName('dynamicbutton')[0];
-
-function hide_menu(){
+// prevent showing responsive elements on different resolution
+function check_size(){
+    // alert(window.innerWidth);
     if(window.innerWidth < 576){
+        // alert('window less than 576px');
         dm.style.display = 'none';
         db.style.display = 'block';
+    }else{
+        // alert('window szerokie');
+        dm.style.display = 'block';
+        db.style.display = 'none';
+    }
+}
+
+let dm = document.getElementsByClassName('left')[0]; 
+let db = document.getElementById('dynamicbutton');
+
+// make page resposive and displays different css grid and toggle button
+function hide_menu(){
+    if(window.innerWidth < 576){
         // alert('window less than 576px');
+        dm.style.display = 'none';
+        db.style.display = 'block';
     }else{
         // alert('window szerokie');
         dm.style.display = 'block';
@@ -16,17 +32,14 @@ function hide_menu(){
 }
 
 // hide show dynamicmenu on click
-// db.addEventListener("click",show_hide_dynamic);
-
-// function show_hide_dynamic(){
-//     if(dm.style.display === "none"){
-//         dm.style.display = "block";
-//     }else{
-//         dm.style.display = "none";
-//     }
-// }
+db.addEventListener("click",show_hide_dynamic);
 
 function show_hide_dynamic(){
+    if(dm.style.display === "none"){
+        // alert('show menu');
         dm.style.display = "block";
-        alert('test');
+    }else{
+        // alert('hide menu');
+        dm.style.display = "none";
     }
+}
